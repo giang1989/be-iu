b860e1494f8027b9a52fd968a1cfde82da417795
giang1989 <86778056+giang1989@users.noreply.github.com>
2021-07-01 18:21:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-    introTitle: 'Linh à!',
+    introTitle: 'Ly à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: '😄😄',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100030313074115' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100064711549965' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
