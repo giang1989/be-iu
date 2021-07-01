48c5a46df3a24ff452782e0eafeb739628c01e3a
giang1989 <86778056+giang1989@users.noreply.github.com>
2021-07-01 20:09:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,4 +15,4 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé baybe.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100064711549965' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100064711549965'//link mess của các bạn. VD: https://m.me/nam.nodemy
